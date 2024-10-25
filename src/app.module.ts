466b49fad249d3c891c2902a3a6623def83c46d1
Abdullohbek11 <145016297+Abdullohbek11@users.noreply.github.com>
2024-10-25 08:15:05 +0500

    aa

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AdminsModule } from './admins/admins.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { TablesModule } from './tables/tables.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    AdminsModule,
+    RestaurantModule,
+    TablesModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
